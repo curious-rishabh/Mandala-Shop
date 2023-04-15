115bf2a2e325caa93ca0226554e72bd9da486877
Curious Rishabh <92927022+curious-rishabh@users.noreply.github.com>
2023-04-15 19:20:53 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+    "use strict";
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+})(jQuery);
